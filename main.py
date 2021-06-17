d49cb1ba90fae7ec3cdfde1d92a827b5ea7c1687
Maria Chashchina <mchashchina@gmail.com>
2021-06-17 13:53:37 +0300

    Added Apple class. Added game logic: snake can eat an aplle and grow, snake can die if it tries to cross it's own tail or leaves the screen. Later I'll check how it was made in the video.

--- main.py
+++ main.py
@@ -4,6 +4,34 @@
 import pygame
 from pygame.locals import *
 import time
+from random import randint
+
+
+SIZE = 40
+
+
+def get_apple_coordinates_multiplier(display_x: int, display_y: int):
+    x = display_x//40-1
+    y = display_y//40-1
+    return x, y
+
+
+class Apple:
+
+    def __init__(self, screen: pygame.display, multipliers):
+        self.apple = pygame.image.load("resourses/apple.jpg").convert()
+        self.screen = screen
+        self.multipliers = multipliers
+        self.x = SIZE*randint(0, self.multipliers[0])
+        self.y = SIZE*randint(0, self.multipliers[1])
+
+    def change_position(self):
+        self.x = SIZE * randint(0, self.multipliers[0])
+        self.y = SIZE * randint(0, self.multipliers[1])
+
+    def draw(self):
+        self.screen.blit(self.apple, (self.x, self.y))
+        pygame.display.flip()
 
 
 class Snake:
@@ -12,50 +40,84 @@ class Snake:
         self.block = pygame.image.load("resourses/block.jpg").convert()
         self.direction = "right"
         self.length = 6
-        self.x = [40]*self.length
-        self.y = [40]*self.length
+        self.x = [SIZE]*self.length
+        self.y = [SIZE]*self.length
         self.screen = screen
 
     def draw(self):
-        self.screen.fill(color=(77, 199, 64))
-        for i in range (self.length):
+        for i in range(self.length):
             self.screen.blit(self.block, (self.x[i], self.y[i]))
         pygame.display.flip()
 
+    def grow(self):
+        self.length += 1
+        self.x += [[]]
+        self.y += [[]]
+
     def move(self):
+        self.screen.fill(color=(77, 199, 64))
         for i in range(self.length-1, 0, -1):
             self.x[i] = self.x[i-1]
             self.y[i] = self.y[i-1]
         if "right" == self.direction:
-            self.x[0] += 40
+            self.x[0] += SIZE
         elif "left" == self.direction:
-            self.x[0] -= 40
+            self.x[0] -= SIZE
         elif "up" == self.direction:
-            self.y[0] -= 40
+            self.y[0] -= SIZE
         else:
-            self.y[0] += 40
+            self.y[0] += SIZE
         self.draw()
 
 
 class SnakeGame:
 
-    def __init__(self):
+    def __init__(self, x=1000, y=800):
         pygame.init()
-        self.surface = pygame.display.set_mode(size=(1000, 700))
+        self.x = x
+        self.y = y
+        self.surface = pygame.display.set_mode(size=(self.x, self.y))
+        self.apple_multiplier = (get_apple_coordinates_multiplier(self.x, self.y))
         self.surface.fill(color=(77, 199, 64))
         self.snake = Snake(self.surface)
-        self.snake.draw()
+        self.apple = Apple(self.surface, self.apple_multiplier)
+        self.running = True
+
+    def play(self):
+        self.snake.move()
+        self.check_obstacles()
+        if self.snake.x[0] == self.apple.x:
+            if self.snake.y[0] == self.apple.y:
+                self.apple.change_position()
+                self.snake.grow()
+        self.apple.draw()
+
+    def check_obstacles(self):
+        for i in range(1, self.snake.length):
+            if self.snake.x[i] == self.snake.x[0]:
+                if self.snake.y[i] == self.snake.y[0]:
+                    self.game_over()
+        if self.snake.x[0] > self.x-SIZE:
+            self.game_over()
+        if self.snake.y[0] > self.y-SIZE:
+            self.game_over()
+        if self.snake.x[0] < 0:
+            self.game_over()
+        if self.snake.y[0] < 0:
+            self.game_over()
+
+    def game_over(self):
+        self.running = False
 
     def run(self):
-        running = True
-        while running:
-            time.sleep(0.5)
+        while self.running:
+            time.sleep(0.3)
             for event in pygame.event.get():
                 if event.type == QUIT:
-                    running = False
+                    self.running = False
                 elif event.type == KEYDOWN:
                     if event.key == K_ESCAPE:
-                        running = False
+                        self.running = False
                     elif event.key == K_UP:
                         self.snake.direction = "up"
                     elif event.key == K_DOWN:
@@ -64,7 +126,7 @@ class SnakeGame:
                         self.snake.direction = "left"
                     elif event.key == K_RIGHT:
                         self.snake.direction = "right"
-            self.snake.move()
+            self.play()
 
 
 if __name__ == "__main__":
